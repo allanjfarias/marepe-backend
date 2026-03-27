44a7f2ee4226853b568c926b40fd603b968c3299
allanjfarias <allferre12@outlook.com>
2026-03-27 08:43:29 -0300

    feat: FastAPI agora aceita conexão do expo

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers.auth import router as auth_router
+
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Minha API FastAPI")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     # Rota raiz
     @app.get("/")
@@ -13,5 +22,6 @@ def create_app() -> FastAPI:
     app.include_router(auth_router, prefix="/auth", tags=["auth"])
 
     return app
+
 
 app = create_app()
