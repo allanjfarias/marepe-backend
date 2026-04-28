7d44b39bdae1594966962343df53b3b8d5f7a1b1
allanjfarias <allferre12@outlook.com>
2026-04-28 04:46:56 -0300

    feat: securit.py para fazer token auth

---
+++ app/core/security.py
@@ -0,0 +1,44 @@
+from fastapi import Header, HTTPException, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from app.core.supabase_client import get_supabase_client
+from app.core.logger import logger
+
+security = HTTPBearer()  
+
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+security = HTTPBearer()
+
+def get_token(
+    credentials: HTTPAuthorizationCredentials = Depends(security)
+) -> str:
+    token = credentials.credentials
+    logger.info(f"Token recebido: {token}")
+
+    if not token:
+        raise HTTPException(status_code=401, detail="Token ausente")
+
+    return token
+
+
+def get_supabase_user(
+    token: str = Depends(get_token),
+    supabase=Depends(get_supabase_client)
+):
+    try:
+        user = supabase.auth.get_user(token)
+
+        if not user or not user.user:
+            raise HTTPException(status_code=401, detail="Usuário inválido")
+
+        return user.user
+
+    except Exception:
+        raise HTTPException(status_code=401, detail="Falha na autenticação")
+
+
+def get_user_id_from_token(
+    user=Depends(get_supabase_user)
+) -> str:
+    return user.id
