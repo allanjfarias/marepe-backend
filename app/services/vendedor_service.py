be9bca899e88df950d80fcdb60b013750ded8544
allanjfarias <allferre12@outlook.com>
2026-05-11 14:59:42 -0300

    fix:  melhorias ao salvar presença

--- app/services/vendedor_service.py
+++ app/services/vendedor_service.py
@@ -14,11 +14,11 @@ def update_vendedor_status(user_id, status, supabase_client):
     try:
         response = (
             supabase_client.table("vendor_presence")
-            .update({
+            .upsert({
+                "vendor_id": user_id,
                 "status": status,
                 "last_seen_at": now_utc()
             })
-            .eq("vendor_id", user_id)
             .execute()
         )
 
@@ -53,12 +53,6 @@ def save_vendedor_location(user_id, latitude, longitude, accuracy, supabase_clie
 
         if not response.data:
             raise HTTPException(500, "Falha ao salvar localização")
-
-        supabase_client.table("vendor_presence").upsert({
-            "vendor_id": user_id,
-            "status": "online",
-            "last_seen_at": now_utc()
-        }).execute()
 
         return response.data[0]
 
